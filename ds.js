d2c618364ed0d605c8fd61e5b5b17cea326f5551
ANKIT SINGH YADAV <95612855+ankitsyadav@users.noreply.github.com>
2022-02-23 10:57:03 +0530

    just to explain
    dsaidasdkasmjlkasdjiojaslndlas

---
+++ ds.js
@@ -0,0 +1,11 @@
+let arr = [2, 4, 1, 5, 4, 9, 7, 5, 2, 3];
+
+for (i = 0; i < arr.length - 1; i++) {
+  for (j = 0; j < arr.length - 1 - i; j++) {
+    if (arr[j] > arr[j + 1]) {
+      [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+    }
+  }
+}
+
+console.log(arr);
