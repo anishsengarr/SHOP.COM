c1648e45f19be2e52152a3f2145905a073a2d440
ImErPratik <99707698+ImErPratik@users.noreply.github.com>
2022-02-25 18:55:02 +0530

    Add files via upload

---
+++ kart page/kart.js/kart.js
@@ -0,0 +1,68 @@
+var data = JSON.parse(localStorage.getItem("kartDataBase"));
+
+console.log(data)
+
+
+data.map(function(elem){
+    console.log(elem)
+
+    var row = document.createElement("tr")
+
+    var td = document.createElement("td")
+    var td1 = document.createElement("td")
+    var td2 = document.createElement("td")
+    var td3 = document.createElement("td")
+
+    var div1 = document.createElement("div")
+    div1.classList.add("mystyle");
+
+    var div2 = document.createElement("div")
+
+
+    var name =document.createElement("p");
+    name.innerText=elem.prodname;
+
+    var price = document.createElement("small");
+    price.value = elem.price;
+
+    var qty = document.createElement("small");
+    qty.innerText = elem.qty;
+
+    var line = document.createElement("br");
+
+    div2.append(name,price,line);
+
+    td.append(div2);
+
+    td1.append(qty);
+
+    var quantity = +elem.qty;
+
+    var prc = +elem.price;
+
+    var total = quantity*prc;
+
+    td2.append(total);
+
+    td3.append(elem.price);
+
+    row.append(td,td1,td3,td2)
+
+    document.querySelector("table").append(row);
+
+    var line = document.createElement("p");
+    line.innerHTML = total;
+
+    // document.querySelector("#sub").append(line);
+    document.querySelector("#sub").append(line);
+
+    document.querySelector("#fnll").append(total);
+
+
+
+})
+
+var sum=0;
+function getSum(total) {
+    return sum+=total ;
+  }
