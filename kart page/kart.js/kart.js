a56df82e433bf942ac48bc36c2cde645fe0d90f1
ImErPratik <99707698+ImErPratik@users.noreply.github.com>
2022-02-26 17:14:12 +0530

    Update kart.js

--- kart page/kart.js/kart.js
+++ kart page/kart.js/kart.js
@@ -6,12 +6,4 @@ console.log(data)
 data.map(function(elem){
     console.log(elem)
 
-    var row = document.createElement("tr")
-
-    var td = document.createElement("td")
-    var td1 = document.createElement("td")
-    var td2 = document.createElement("td")
-    var td3 = document.createElement("td")
-
-    var div1 = document.createElement("div")
     
